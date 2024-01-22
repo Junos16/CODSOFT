7d624dfe0d5c1f86f20d1633e39cc2f23fc3f9b0
Junos16 <hriddhitdatta2002@gmail.com>
2024-01-22 12:22:21 +0530

    Add program for number guessing game

---
+++ noGuessGame.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <random>
+
+int main() {
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist100(1, 100);
+    
+    int target = dist100(rng);
+    
+    while(true) {
+        std::cout << "Guess the number (between 1 and 100): ";
+        int input;
+        std::cin >> input;
+
+        if (input == target) {
+            std::cout << "Correct!" << std::endl;
+            break;
+        }
+
+        else if (input > target) {
+            std::cout << "Wrong!, the number is less than " << input << std::endl;
+        }
+
+        else {
+            std::cout << "Wrong!, the number is greater than " << input << std::endl;
+        }
+    }   
+    return 0;
+}
